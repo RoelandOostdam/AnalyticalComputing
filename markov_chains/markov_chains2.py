d4d877d123ea1d5f9df411a1f05af2baaa4b7fb0
Roeland <roeland.oostdam@outlook.com>
2020-03-30 02:38:28 +0200

    Added state probability plot

--- markov_chains/markov_chains2.py
+++ markov_chains/markov_chains2.py
@@ -13,17 +13,28 @@ class Flumph():
 		self.state_names = ["Hungry", "Satisfied", "Scared", "Salty"]
 		self.vector = [0.7,0.1,0.1,0.1]
 
-	def get_chances(self, steps=1, vebose=True):
+	def get_chances(self, steps=1):
+		timeline = np.array([self.vector])
 		for x in range(steps):
 			self.vector = np.sum(self.matrix.T * self.vector, axis=1)
+			timeline = np.concatenate((timeline, [self.vector]))
 
-		if vebose:
-			print("Result after", steps, "steps")
-			print("-"*30)
-			for i, x in enumerate(self.vector):
-				print(self.state_names[i],":", round(x,3), "%")
+		print("Result after", steps, "steps")
+		print("-"*30)
+		for i, x in enumerate(self.vector):
+			print(self.state_names[i],":", round(x,3), "%")
+
+		for x in range(0,len(self.state_names)):
+			plt.plot(range(0,steps+1),timeline.take(x,axis=1), label=self.state_names[x])
+		plt.title("Markov Chain State Probability")
+		plt.xlabel("Step")
+		plt.ylabel("Probability (%)")
+
+		plt.legend()
+		plt.grid()
+		plt.show()
 
 		return self.vector
 
 f = Flumph()
-f.get_chances(10)
+f.get_chances(100)
